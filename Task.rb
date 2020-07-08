c124f1dbd67ccf7715551e577f28ba1f399ddfae
Vitaliy V. Shopov <shopov.vitaliy@gmail.com>
2020-07-08 16:17:24 +1000

    Remove singlton pattern

--- Task.rb
+++ Task.rb
@@ -13,7 +13,6 @@ require 'json'
 require 'date'
 require 'minitest/autorun'
 require 'set'
-require 'singleton'
 
 class User
   attr_reader :attributes, :sessions
@@ -25,11 +24,17 @@ class User
 end
 
 class Reporter
-  include Singleton
+  def initialize
+    @report = {
+        'totalUsers' => 0,
+        'uniqueBrowsersCount' => 0,
+        'totalSessions' => 0,
+        'allBrowsers' => Set.new,
+        'usersStats' => {}
+    }
+  end
 
   def generate(source_path, target_path)
-    reset
-
     File.open(source_path).each do |line|
       cols = line.split(',').map(&:strip)
       kind = cols.shift
@@ -42,16 +47,6 @@ class Reporter
   end
 
   private
-
-  def reset
-    @report = {
-      'totalUsers' => 0,
-      'uniqueBrowsersCount' => 0,
-      'totalSessions' => 0,
-      'allBrowsers' => Set.new,
-      'usersStats' => {}
-    }
-  end
 
   def process_user(fields)
     if @report['usersStats'].has_key?(fields[0])
@@ -291,7 +286,8 @@ session,2,3,Chrome 20,84,2016-11-25
   def test_new_result
     File.truncate('result_new.json', 0) if File.exist?('result_new.json')
 
-    Reporter.instance.generate('data.txt', 'result_new.json')
+    reporter = Reporter.new
+    reporter.generate('data.txt', 'result_new.json')
 
     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"age":"0","sessionsCount":6,"browsers":"FIREFOX 12,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 29,SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"totalTime":"455 min.","longestSession":"118 min.","dates":"2017-09-27,2017-03-28,2017-02-27,2016-10-23,2016-09-15,2016-09-01"},"Palmer Katrina":{"age":"65","sessionsCount":5,"browsers":"CHROME 13,CHROME 6,FIREFOX 32,INTERNET EXPLORER 10,SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"totalTime":"218 min.","longestSession":"116 min.","dates":"2017-04-29,2016-12-28,2016-12-20,2016-11-11,2016-10-21"},"Gregory Santos":{"age":"86","sessionsCount":4,"browsers":"CHROME 20,CHROME 35,FIREFOX 47,SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"totalTime":"192 min.","longestSession":"85 min.","dates":"2018-09-21,2018-02-02,2017-05-22,2016-11-25"}}}' + "\n"
     assert_equal expected_result, File.read('result_new.json')
@@ -301,7 +297,8 @@ session,2,3,Chrome 20,84,2016-11-25
     File.truncate('result_new_large.json', 0) if File.exist?('result_new_large.json')
 
     beginning_time = Time.now
-    Reporter.instance.generate('data_large.txt', 'result_new_large.json')
+    reporter = Reporter.new
+    reporter.generate('data_large.txt', 'result_new_large.json')
     end_time = Time.now
 
     puts "[Test New Large]: Time elapsed #{(end_time - beginning_time)*1000} ms"
